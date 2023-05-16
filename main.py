f4866122c38d50df07b5d323a3aa38e9e00db34e
Ruben-GV <ardopro520@gmail.com>
2023-05-16 00:58:40 -0500

    implementacion de chatGPT

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import os
+import openai
 from dotenv import load_dotenv
 
 load_dotenv()
 
 api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = api_key
+
+#modelos = openai.Model.list()
+#print(modelos)
+
+modelo = "text-davinci-002"
+prompt = "¿Cúal es la capital de Francia?"
+#prompt = input("Ingresa tu pregunta: ")
+
+respuesta = openai.Completion.create(
+    engine=modelo,
+    prompt=prompt,
+    n=1 #Numero de respuestas que queremos
+)
+texto_generado = respuesta.choices[0].text.strip()
+print(texto_generado)
