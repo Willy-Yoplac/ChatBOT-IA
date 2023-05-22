683371fa9aabe080399b8c7e19e9a36445b2d149
Willy-Yoplac <jovan2000al@gmail.com>
2023-05-22 18:29:23 -0500

    Puede dar respuestas multiples

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ openai.api_key = api_key
 #print(modelos)
 
 modelo = "text-davinci-002"
-prompt = "¿Cúal es la capital de Francia?"
+prompt = "Elija un buen nombre para un perrito"
 #prompt = input("Ingresa tu pregunta: ")
+
+#Enviamos la peticon
 
 respuesta = openai.Completion.create(
     engine=modelo,
     prompt=prompt,
-    n=1 #Numero de respuestas que queremos
+    n=3, #Numero de respuestas que queremos
+    temperature =1, # que tan aleatorio seran las respuestas
+    max_tokens = 50
 )
-texto_generado = respuesta.choices[0].text.strip()
-print(texto_generado)
+
+for idx, opcion in enumerate(respuesta.choices):
+    text_generado = opcion.text.strip()
+    print(f'Respuesta {idx + 1}: {text_generado}\n')
