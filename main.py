0c9904a8c71463c4017cba4637b0049b791e89e3
Willy-Yoplac <jovan2000al@gmail.com>
2023-05-28 00:32:19 -0500

    Procesamiento del lenguage con spaCy (revisar la 2da pregunta)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import spacy
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,19 +12,42 @@ openai.api_key = api_key
 #print(modelos)
 
 modelo = "text-davinci-002"
-prompt = "Escribe oraciones con la palabra 'casa'."
-#prompt = input("Ingresa tu pregunta: ")
+prompt = "Cuentame una historia breve sobre un viaje a un pais europeo"
 
 #Enviamos la peticon
 
 respuesta = openai.Completion.create(
     engine=modelo,
     prompt=prompt,
-    n=2, #Numero de respuestas que queremos
-    temperature =0.5, # que tan aleatorio seran las respuestas
+    n=1, #Numero de respuestas que queremos
+   # temperature =1, # que tan aleatorio seran las respuestas
     max_tokens = 100
 )
 
-for idx, opcion in enumerate(respuesta.choices):
-    text_generado = opcion.text.strip()
-    print(f'Respuesta {idx + 1}: {text_generado}\n')
+texto_generado = respuesta.choices[0].text.strip()
+print(texto_generado)
+
+print("*"*30) # diferenciar la respuesta tratada
+
+# modelo que procesa informacion en español
+modelo_spacy = spacy.load("es_core_news_md")
+analisis = modelo_spacy(texto_generado)
+
+# paraque generar una pregunta sobre la entidad reconocida
+ubicacion = None
+for ent in analisis.ents:
+    if ent.label_ == "LOC":
+        ubicacion = ent
+        break
+
+# si existe la ubicacion
+if ubicacion:
+    prompt2 = f"Dime mas acreca de {ubicacion}"
+    respuesta2 = openai.Completion.create(
+        engine = modelo,
+        prompt = prompt2,
+        n=1,
+        max_tokens =100
+    )
+    print(respuesta2.choises[0].text.strip())
+
